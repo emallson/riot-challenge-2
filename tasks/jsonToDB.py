83ad8aaef78a0e0fb19af3a921e884fd55f6667a
Impally <Rodger.Friesen@gmail.com>
2015-08-18 18:07:27 -0400

    Updated scripts to insert into the DB, FailedFiles.txt is a list of all files that failed to insert for some unknown reason.

--- tasks/jsonToDB.py
+++ tasks/jsonToDB.py
@@ -18,7 +18,7 @@ files = glob.glob(path)
 for file1 in files: 
     #dirs = os.listdir( dirPath )
     file2 = glob.glob(file1)
-
+    myfile = open("FailedFiles.txt", "a")
     for name in file2: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
         try:
             with open(name, encoding="utf8") as f: # No need to specify 'r': this is the default.
@@ -26,10 +26,11 @@ for file1 in files:
                 json_data=f
                 data = json.load(json_data)
                 data["_id"] = "match" + str(data["matchId"])
-                if not db.get(data["_id"]):
-                    db.save(data)
+                db.save(data)
                 json_data.close()
                 #time.sleep(2)
         except IOError as exc:
-                if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
-                    raise # Propagate other kinds of IOError.
+            if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
+                raise # Propagate other kinds of IOError.
+        except:
+            myfile.write(name + '\n')
