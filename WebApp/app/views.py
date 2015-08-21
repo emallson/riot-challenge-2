16869430466bc7d3dae86de7d76baee7009981bc
Impally <Rodger.Friesen@gmail.com>
2015-08-20 22:07:14 -0400

    Fixed views, adding a view to the website and testing that now.

--- WebApp/app/views.py
+++ WebApp/app/views.py
@@ -1,8 +1,18 @@
 from flask import render_template, flash, redirect, g, request
 from app import app
-from .forms import LoginForm
 from werkzeug.local import LocalProxy
 couch = LocalProxy(lambda: g.couch)
+
+def getChampName(champID):
+	champion_name_view = ViewDefinition('champions', 'champName', '''\
+              function(doc) 
+                      { 
+                        Object.keys(doc).forEach(function(k) 
+                          { emit(k, doc[k].name); }); }"
+		''')
+	manager.add_viewdef(champion_name_view)
+	champKeys = champion_name_view()
+	return champKeys[1]
 
 @app.route('/')
 @app.route('/index')
@@ -15,4 +25,4 @@ def index():
 @app.route('/view_profile')
 def view_profile():
 	document = couch.get("25023dc39a5f00c52fb91eb35b000e91")
-	return "This works!"
+	return getChampName(1)
